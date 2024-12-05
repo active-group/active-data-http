826df1676259443578c2b04eb65fac27fa77710d
David Frese <david.frese@active-group.de>
2024-12-05 15:05:12 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.active-group/active-data-http "1.0.0-SNAPSHOT"
+(defproject de.active-group/active-data-http "0.1.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
